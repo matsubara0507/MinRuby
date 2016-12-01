110b4f8a7583816a9c7e67c9cc7a406296b13329
松原信忠 <t12307043@gunma-u.ac.jp>
2016-12-01 23:59:39 +0900

    Created interp

---
+++ exercise/interp.rb
@@ -0,0 +1,31 @@
+require "minruby"
+
+def evaluate(tree)
+  case tree[0]
+  when "lit"
+    tree[1]
+  when "+"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left + right
+  when "-"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left - right
+  when "*"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left * right
+  else
+    # ここでは tree[0] == "/"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left / right
+  end
+end
+
+str = gets
+tree = minruby_parse(str)
+answer = evaluate(tree)
+
+p(answer)
