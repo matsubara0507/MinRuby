1f54f9ec4028cbcbde535d55816ab5edd0e7579d
松原信忠 <t12307043@gunma-u.ac.jp>
2016-12-02 00:36:14 +0900

    Solved exercise on term 4

--- exercise/interp.rb
+++ exercise/interp.rb
@@ -16,11 +16,56 @@ def evaluate(tree)
     left  = evaluate(tree[1])
     right = evaluate(tree[2])
     left * right
+  when "%"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left % right
+  when "**"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left ** right
+  when "=="
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left == right
+  when "!="
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left != right
+  when "<="
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left <= right
+  when ">="
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left >= right
+  when "<"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left < right
+  when ">"
+    left  = evaluate(tree[1])
+    right = evaluate(tree[2])
+    left > right
   else
     # ここでは tree[0] == "/"
     left  = evaluate(tree[1])
     right = evaluate(tree[2])
     left / right
+  end
+end
+
+def max(tree)
+  case tree[0]
+  when "lit"
+    tree[1]
+  when "+","-","*","/","%","**","==","!=","<=",">=","<",">"
+    left  = max(tree[1])
+    right = max(tree[2])
+    [left,right].max
+  else
+    tree.max
   end
 end
 
