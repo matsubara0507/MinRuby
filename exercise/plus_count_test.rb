70a37a598df5d3b30dac0a556adc83475e8b47ab
松原信忠 <t12307043@gunma-u.ac.jp>
2016-12-03 02:56:13 +0900

    Solved exercise on term 5

---
+++ exercise/plus_count_test.rb
@@ -0,0 +1,5 @@
+plus_count = 0
+x = 1 + 2 + 3
+p(plus_count) #=> 2
+x = 1 + 2 + 3
+p(plus_count) #=> 4
---
+++ exercise/term5.rb
@@ -0,0 +1,53 @@
+require "minruby"
+
+def evaluate(tree, env)
+  case tree[0]
+  when "lit"
+    tree[1]
+  when "+"
+    env["plus_count"] += 1
+    evaluate(tree[1], env) + evaluate(tree[2], env)
+  when "-"
+    evaluate(tree[1], env) - evaluate(tree[2], env)
+  when "*"
+    evaluate(tree[1], env) * evaluate(tree[2], env)
+  when "/"
+    evaluate(tree[1], env) / evaluate(tree[2], env)
+  when "%"
+    evaluate(tree[1], env) % evaluate(tree[2], env)
+  when "**"
+    evaluate(tree[1], env) ** evaluate(tree[2], env)
+  when "<"
+    evaluate(tree[1], env) < evaluate(tree[2], env)
+  when "<="
+    evaluate(tree[1], env) <= evaluate(tree[2], env)
+  when "=="
+    evaluate(tree[1], env) == evaluate(tree[2], env)
+  when ">="
+    evaluate(tree[1], env) >= evaluate(tree[2], env)
+  when ">"
+    evaluate(tree[1], env) > evaluate(tree[2], env)
+  when "func_call" # 仮の実装
+    p(evaluate(tree[2], env))
+  when "stmts"
+    i = 1
+    last = nil
+    while tree[i]
+      last = evaluate(tree[i], env)
+      i = i + 1
+    end
+    last
+  when "var_assign"
+    env[tree[1]] = evaluate(tree[2], env)
+  when "var_ref"
+    env[tree[1]]
+  else
+    puts("error")
+    pp(tree)
+    raise("unknown node")
+  end
+end
+
+str = minruby_load()
+tree = minruby_parse(str)
+answer = evaluate(tree,{ "plus_count" => 0 })
